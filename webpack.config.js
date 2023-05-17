e68b2a43c1bef2a891b345fce6083b24462fcef0
성연철 <s9292909@gmail.com>
2023-05-18 00:03:54 +0900

    webpack config fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
 const path = require("path");
-
+const webpack = require("webpack");
 module.exports = {
   mode: "development",
   entry: {
-    main: "./src/app.js"
+    main: "./src/app.js",
   },
   output: {
     filename: "[name].js",
-    path: path.resolve("./dist")
+    path: path.resolve("./dist"),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
         loader: "url-loader",
         options: {
           name: "[name].[ext]?[hash]",
-          limit: 10000 // 10Kb
-        }
-      }
-    ]
-  }
+          limit: 10000, // 10Kb
+        },
+      },
+    ],
+  },
   /**
    * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
    * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요.
@@ -32,4 +32,9 @@ module.exports = {
    * 3. CleanWebpackPlugin: 빌드 전에 아웃풋 폴더를 깨끗히 정리하세요.
    * 4. MiniCssExtractPlugin: 모듈에서 css 파일을 분리하세요.
    */
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `Build Tile: ${new Date().toDateString}`,
+    }),
+  ],
 };
